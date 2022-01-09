ca1d66292efcde02fd3bf6e72a52be5c634d1743
REXIANN <gandolkim@gmail.com>
2022-01-09 23:11:24 +0900

    css imported _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,11 @@
 import * as React from 'react'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
       <Component />
       <span>Hiii</span>
-      <style jsx global>{`
-        a {
-          color: blue;
-        }
-      `}</style>
     </div>
   )
 }
