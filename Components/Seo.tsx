f1c1c37a8fd7a32b897a94501b4ad94171a08efb
rexiann <gandolkim@gmail.com>
2022-03-07 01:18:35 +0900

    installed by pnpm

--- Components/Seo.tsx
+++ Components/Seo.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
 import Head from 'next/head'
 
+interface Props {
+  title: string;
+}
 // meta descriptions, author, etc
-export default function Seo({ title }) {
+export default function Seo({ title }: Props) {
   return (
   <Head>
     <title>{title} | Next Movies</title>
